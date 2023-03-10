50154b1d88408613cc0e7cb201353b2e6da10b1c
Huong <huongnguyen.nth1709@gmail.com>
2023-03-10 19:29:18 +0200

    implement Login component - googleLogin

---
+++ frontend-react/src/components/Login.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { GoogleLogin } from '@react-oauth/google';
+import jwt_decode from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
+
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logowhite.png';
+
+import { client } from '../client';
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const googleSuccess = (response) => {
+    console.log(response);
+    const decoded = jwt_decode(response.credential);
+    const { name, picture, sub } = decoded;
+    localStorage.setItem('user', JSON.stringify(decoded));
+
+    const doc = {
+      _id: sub,
+      _type: 'user',
+      userName: name,
+      image: picture,
+    };
+
+    client.createIfNotExists(doc).then(() => {
+      navigate('/', { replace: true });
+    });
+  };
+
+  const googleFailure = (error) => {
+    console.log('Error', error);
+  };
+
+  return (
+    <div className='flex justify-start items-center flex-col h-screen'>
+      <div className='relative w-full h-full'>
+        <video
+          src={shareVideo}
+          type='video/mp4'
+          loop
+          muted
+          autoPlay
+          className='w-full h-full object-cover'
+        />
+
+        <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+          <div className='p-5'>
+            <img src={logo} alt='logo' width='130px' />
+          </div>
+
+          <div className='shadow-2xl'>
+            <GoogleLogin
+              className='bg-mainColor flex justify-center item-center p-3 rounded-lg cursor-pointer outline-none'
+              onSuccess={(response) => googleSuccess(response)}
+              onError={(error) => googleFailure(error)}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
