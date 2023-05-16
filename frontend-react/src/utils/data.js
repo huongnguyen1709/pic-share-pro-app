e5e946fc191c2efc14832d441afaab54e2048445
Huong <huongnguyen.nth1709@gmail.com>
2023-05-16 19:57:16 +0300

    add userCreatedPinsQuery

--- frontend-react/src/utils/data.js
+++ frontend-react/src/utils/data.js
@@ -182,3 +182,28 @@ export const pinDetailMorePinQuery = (pin) => {
   }`;
   return query;
 };
+
+export const userCreatedPinsQuery = (userId) => {
+  const query = `*[ _type == 'pin' && userId == '${userId}'] | order(_createdAt desc){
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+  return query;
+};
